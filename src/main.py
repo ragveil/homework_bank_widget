4bb34f91bd989f4b024686d6b618f96cb0c84273
Nadezhda Kornienko <service911@bk.ru>
2025-04-08 23:56:57 +1000

    feat: add bonus function with card and account generation

--- src/main.py
+++ src/main.py
@@ -1,25 +1,46 @@
 import random
 from string import digits
+from typing import Any
 
 from src.masks import get_mask_account, get_mask_card_number
 from src.widget import get_date, mask_account_card
 
+CARD_VARIANTS = ("Maestro", "MasterCard", "Visa Classic", "Visa", "Visa Platinum", "Visa Gold", "МИР")
+TYPES_OF_NUMS = ("card", "account")
+
 
 def bonus_generate_random(nums: str) -> str:
-    """Бонусная функция.
-    Принимает значения 'card' и 'account', генерирует номер карты или счета и возвращает в виде маски"""
-    resp_num = "Некорректные данные"
-    if nums == "card":
-        generate_num = "".join(random.choices(digits, k=16))
-        resp_num = get_mask_card_number(generate_num)
-    elif nums == "account":
-        generate_num = "".join(random.choices(digits, k=20))
-        resp_num = get_mask_account(generate_num)
-    return resp_num
+    """
+    Бонусная функция для дополнительной проверки функции mask_account_card из модуля widget и наработка навыка автора.
+    :param nums: Входящий тип запроса пользователя
+    :return: Строка
+    """
+    if nums in TYPES_OF_NUMS:
+        x: Any = lambda len_nums: "".join(random.choices(digits, k=len_nums))
+        if nums == "card":
+            return mask_account_card(f"{random.choice(CARD_VARIANTS)} {x(16)}")
+        elif nums == "account":
+            return mask_account_card(f"Счет {x(20)}")
+    return "Некорректные данные"
 
 
-print(bonus_generate_random("card"))
-print(bonus_generate_random("account"))
-print(mask_account_card("Visa Gold 5999414228426353"))
-print(mask_account_card("Счет 73654108430135874305"))
-print(get_date("2024-03-11T02:26:18.671407"))
+print(
+    f"""Проверка работы бонусной функции:
+Карта: {bonus_generate_random("card")}
+Счет: {bonus_generate_random("account")}""",
+    end="\n\n",
+)
+
+print(
+    f"""Проверка работоспособности функций модуля widget в рамках второй домашней работы:
+Карта: {mask_account_card("Visa Gold 5999414228426353")}
+Счет: {mask_account_card("Счет 73654108430135874305")}
+Дата: {get_date("2024-03-11T02:26:18.671407")}""",
+    end="\n\n",
+)
+
+print(
+    f"""Проверки работоспособности функций модуля masks из первой домашней работы:
+Карта: {get_mask_card_number('7000792289606361')}
+Счет: {get_mask_account('73654108430135874305')}"""
+)
