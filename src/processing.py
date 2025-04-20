dfd3a365132c99886f0da598ef981da8d5d54040
Nadezhda Kornienko <service911@bk.ru>
2025-04-20 21:38:10 +1000

    feat: sort_by_date function into processing.py module

--- src/processing.py
+++ src/processing.py
@@ -6,3 +6,13 @@ def filter_by_state(list_of_dicts: list[dict[str, int | str]], state: str = "EXE
     :return: Список словарей, соответствующих критерию фильтрации.
     """
     return list(filter(lambda d: d.get("state") == state, list_of_dicts))
+
+
+def sort_by_date(list_of_dicts: list[dict[str, str | int]], sort: bool = True) -> list[dict[str, str | int]] | None:
+    """
+    Сортирует операции в соответствующем порядке.
+    :param list_of_dicts: Входящее значение в виде списка словарей.
+    :param sort: Значение для сортировки по умолчанию типа bool.
+    :return: Список словарей, отсортированных в указанном порядке.
+    """
+    return sorted(list_of_dicts, key=lambda d: str(d.get("date")), reverse=sort)
