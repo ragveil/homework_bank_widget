6540a957e66231979537a03ef387119abae09c7f
Nadezhda Kornienko <service911@bk.ru>
2025-05-06 11:19:05 +1000

    refactor: move constants to separate file constants.py

--- src/main.py
+++ src/main.py
@@ -2,19 +2,10 @@ import random
 from string import digits
 from typing import Any
 
+from src.constants import BANKING_OPERATIONS, CARD_VARIANTS, TYPES_OF_NUMS
 from src.masks import get_mask_account, get_mask_card_number
 from src.processing import filter_by_state, sort_by_date
 from src.widget import get_date, mask_account_card
-
-CARD_VARIANTS = ("Maestro", "MasterCard", "Visa Classic", "Visa", "Visa Platinum", "Visa Gold", "МИР")
-TYPES_OF_NUMS = ("card", "account")
-
-BANKING_OPERATIONS = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
 
 
 def bonus_generate_random(nums: str) -> str:
@@ -40,6 +31,13 @@ print(
 )
 
 print(
+    f"""Проверки работоспособности функций модуля masks из первой домашней работы:
+Карта: {get_mask_card_number('7000792289606361')}
+Счет: {get_mask_account('73654108430135874305')}""",
+    end="\n\n",
+)
+
+print(
     f"""Проверка работоспособности функций модуля widget в рамках второй домашней работы:
 Карта: {mask_account_card("Visa Gold 5999414228426353")}
 Счет: {mask_account_card("Счет 73654108430135874305")}
@@ -48,16 +46,10 @@ print(
 )
 
 print(
-    f"""Проверки работоспособности функций модуля masks из первой домашней работы:
-Карта: {get_mask_card_number('7000792289606361')}
-Счет: {get_mask_account('73654108430135874305')}""",
-    end="\n\n",
-)
-
-print(
     f"""Проверка работоспособности функций модуля processing в рамках третьей домашней работы:
 Фильтрация по состоянию по умолчанию: {filter_by_state(BANKING_OPERATIONS)}
 Фильтрация по состоянию с использованием дополнительного аргумента: {filter_by_state(BANKING_OPERATIONS, 'CANCELED')}
 Сортировка даты по умолчанию: {sort_by_date(BANKING_OPERATIONS)}
-Сортировка даты с использованием дополнительного аргумента: {sort_by_date(BANKING_OPERATIONS, False)}"""
+Сортировка даты с использованием дополнительного аргумента: {sort_by_date(BANKING_OPERATIONS, False)}""",
+    end="\n\n",
 )
