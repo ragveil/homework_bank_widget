8219f492ca436a1ad9390cdbbf9bddf9a731d0dd
Nadezhda Kornienko <service911@bk.ru>
2025-05-22 21:51:45 +1000

    feat: add utils.py module with get_transactions() function

--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,21 @@
-# Функция, которая принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях.
-# Если файл пустой, содержит не список или не найден, функция возвращает пустой список.
+import json
+import os
+from typing import Any
+
+from config import ROOT_DIR
+
+operations_path = os.path.join(ROOT_DIR, "data\\")
+
+
+def get_transactions(path: str) -> Any:
+    """
+    Конвертирует данные из JSON-файла в объект Python со словарями о транзакциях.
+    :param path: Входящее значение в виде пути до файла.
+    :return: Результат работы функции, список.
+    """
+    try:
+        with open(path, "r", encoding="utf-8") as f:
+            operations_data = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        operations_data = []
+    return operations_data
