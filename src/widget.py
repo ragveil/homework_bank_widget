b09849b477da74e0be2ea66e406325a2b297af9a
Nadezhda Kornienko <service911@bk.ru>
2025-06-03 22:31:46 +1000

    update: update widget module for correct work with new main.py functionality

--- src/widget.py
+++ src/widget.py
@@ -1,13 +1,14 @@
 import re
 from datetime import datetime
+from typing import Any
 
 from src.masks import get_mask_account, get_mask_card_number
 
 
-def mask_account_card(both: str) -> str:
+def mask_account_card(both: Any) -> str:
     """
     Маскирует данные банковских карт или счета.
-    :param both: Входящее значение в виде строки с текстом и номером карты или счета.
+    :param both: Входящее значение.
     :return: Строка с использованием маски вида XXXX XX** **** XXXX или **ХХ.
     """
     if card := re.search(r"(\s\d{16}$)", both):
@@ -18,14 +19,14 @@ def mask_account_card(both: str) -> str:
         return "Некорректный номер."
 
 
-def get_date(date: str) -> str:
+def get_date(date: Any) -> str:
     """
     Форматирование даты и времени в корректный вид.
 
-    :param date: Входящее значение в виде строки формата дата-время.
+    :param date: Входящее значение.
     :return: Строка со значением даты формата день.месяц.год
     """
-    if re.match(r"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{6}", date):
+    if re.match(r"^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}", date):
         return datetime.fromisoformat(date).strftime("%d.%m.%Y")
     else:
         return "Некорректный формат даты."
